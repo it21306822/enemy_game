3c68ef27244f7faf5c1c23f0ed12478b40575c8a
S.Nallaperuma <it21306822@my.sliit.lk>
2024-09-08 02:58:28 +0530

    Create initial window setup using Bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use bevy::prelude::*;
+use bevy::window::WindowResolution;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::srgb(0.04, 0.04, 0.04)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Rust Invaders!".to_string(),
+                resolution: WindowResolution::new(598.0, 676.0),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        
+        .run();
+}
+
+fn setup_system(mut commands: Commands) {
+    // Add setup logic here, if needed
 }
